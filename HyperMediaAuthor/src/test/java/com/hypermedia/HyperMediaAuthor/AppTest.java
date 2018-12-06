d237ce28ecf537c0d60b22b47aaa449a3db970e3
Jungbom Pak <jungbompak@Jungboms-MacBook-Pro.local>
2018-12-06 13:25:11 -0800

    Disable unit test

--- HyperMediaAuthor/src/test/java/com/hypermedia/HyperMediaAuthor/AppTest.java
+++ HyperMediaAuthor/src/test/java/com/hypermedia/HyperMediaAuthor/AppTest.java
@@ -1,20 +1,20 @@
 package com.hypermedia.HyperMediaAuthor;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+///**
+// * Unit test for simple App.
+// */
+//public class AppTest 
+//{
+//    /**
+//     * Rigorous Test :-)
+//     */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+//}
